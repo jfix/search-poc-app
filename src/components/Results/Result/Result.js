7053bfc1f38865c20e8d59720ad7595b1e55dcc8
Jakob Fix <jakob.fix@gmail.com>
2022-10-17 00:52:05 +0200

    Improve display of results

--- src/components/Results/Result/Result.js
+++ src/components/Results/Result/Result.js
@@ -11,35 +11,52 @@ export default function Result(props) {
     var size = props.document.size;
     size = (size === '1 pages') ? 'One page' : size;
     var imageUrl = props.document.image_url.replace('https:https://', 'https://')
+    var subjectArray = (language === 'English') ? props.document.subjects_en : props.document.subjects_fr;
+
+    const subjects = subjectArray.map(s => {
+        return (
+            <span style={{ marginRight: "0.3em", fontSize: "0.6em" }} className="badge rounded-pill bg-primary">{s}</span>
+        )
+    })
+
+    // ugly fix
     imageUrl = (imageUrl === 'https:') ? `https://via.placeholder.com/340x460?text=${props.document.type}` : imageUrl
-    return(
-        <div className="card p-0 result mb-12" style={{"width": "100%", "minWidth": "100%"}}>
+
+    return (
+        <div className="card p-0 result mb-12" style={{ "width": "100%", "minWidth": "100%" }}>
             <div className="row">
                 <div className="col-md-2">
-                    <a href={`/details/${props.document.id}`} title="Click for details">
+                    <a href={`/details/${props.document.id}`} rel="noreferrer" target="_blank" title="Click for details">
                         <img src={imageUrl} alt={props.document.title_en} className="result-img img-fluid rounded-start"></img>
                     </a>
                 </div>
                 <div className="col-md-10">
-                <div className="card-body">
-                    <h5 className="card-title">{props.document.title_en || props.document.title_fr}</h5>
-                    <p className="text-truncate card-text">{description}</p>
-                    <p className="card-text"><a href={props.document.url}>{props.document.url}</a></p>
-                    <div className='row'>
-                        <div className='col-md-2'>
-                            <p className="card-text"><small className="text-capitalize text-muted">{props.document.type}</small></p>
+                    <div className="card-body">
+                        <h5 className="card-title">{props.document.title_en || props.document.title_fr}</h5>
+                        <p className="text-truncate card-text">{description}</p>
+                        <p className="card-text">
+                            <a href={props.document.url}>{props.document.url}</a>
+                        </p>
+                        <div className='row'>
+                            <div className='col-md-3'>
+                                <p className="card-text"><small className="text-capitalize text-muted">{props.document.type}</small></p>
+                            </div>
+                            <div className='col-md-3'>
+                                <p className="card-text"><small className="text-muted">{dayjs(props.document.date).format('D MMM YYYY')}</small></p>
+                            </div>
+                            <div className='col-md-3'>
+                                <p className="card-text"><small className="text-muted">{language}</small></p>
+                            </div>
+                            <div className='col-md-3'>
+                                <p className="card-text"><small className="text-muted">{size}</small></p>
+                            </div>
                         </div>
-                        <div className='col-md-2'>
-                            <p className="card-text"><small className="text-muted">{dayjs(props.document.date).format('D MMM YYYY')}</small></p>
-                        </div>
-                        <div className='col-md-2'>
-                            <p className="card-text"><small className="text-muted">{language}</small></p>
-                        </div>
-                        <div className='col-md-2'>
-                            <p className="card-text"><small className="text-muted">{size}</small></p>
+                        <div className='row'>
+                            <div className='col-md-12'>
+                                {subjects}
+                            </div>
                         </div>
                     </div>
-                </div>
                 </div>
             </div>
         </div>
